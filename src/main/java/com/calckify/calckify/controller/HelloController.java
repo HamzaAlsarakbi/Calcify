7bf7adf9c2ae4d478ebbe11e0bc5e59d60d65e2c
Electr0d <hamoz.sq78@gmail.com>
2022-12-25 13:38:51 -0500

    separated into mvc

--- src/main/java/com/calckify/calckify/controller/HelloController.java
+++ src/main/java/com/calckify/calckify/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.calckify.calckify;
+package com.calckify.calckify.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,4 +7,6 @@ module com.calckify.calckify {
 
     opens com.calckify.calckify to javafx.fxml;
     exports com.calckify.calckify;
+    exports com.calckify.calckify.controller;
+    opens com.calckify.calckify.controller to javafx.fxml;
 }
