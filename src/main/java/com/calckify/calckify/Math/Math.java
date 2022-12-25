c27a34962228308b6b4ae4201ce70ca02c10b4c7
Saad Sheikh <saad1662002@gmail.com>
2022-12-25 17:57:44 -0500

    Fixed major string formatting flaw for 3 formats of opening and closing brackets

--- src/main/java/com/calckify/calckify/Math/Math.java
+++ src/main/java/com/calckify/calckify/Math/Math.java
@@ -16,39 +16,42 @@ public class Math {
             modInput.append('(');
             int i = 2;
             int validityBool = 0;
-            while(!input.endsWith(")\\")){
-                if(i >= input.length()){
-                    validityBool = 1;
-                    break;
+            if(input.endsWith(")\\")){
+                while(i < input.length()){
+                    modInput.append(input.charAt(i));
+                    i++;
                 }
-                modInput.append(input.charAt(i));
-                i++;
+            }
+            else{
+                validityBool = 1;
             }
         }
         else if(input.startsWith("$")){ // for expressions starting with '$' in format of $...$
             modInput.append('(');
             int i = 1;
             int validityBool = 0;
-            while(!input.endsWith("$")){
-                if(i >= input.length()){
-                    validityBool = 1;
-                    break;
+            if(input.endsWith("$")){
+                while(i < input.length()){
+                    modInput.append(input.charAt(i));
+                    i++;
                 }
-                modInput.append(input.charAt(i));
-                i++;
+            }
+            else{
+                validityBool = 1;
             }
         }
         else if(input.startsWith("\\begin{math}")){ // for expressions starting with '\begin{math}' in format of \begin{math}...\end{math}
             modInput.append('(');
             int i = 12;
             int validityBool = 0;
-            while(!input.endsWith("\\end{math}")){
-                if(i >= input.length()){
-                    validityBool = 1;
-                    break;
+            if(input.endsWith("\\end{math}")){
+                while(i < input.length()){
+                    modInput.append(input.charAt(i));
+                    i++;
                 }
-                modInput.append(input.charAt(i));
-                i++;
+            }
+            else{
+                validityBool = 1;
             }
         }
 
